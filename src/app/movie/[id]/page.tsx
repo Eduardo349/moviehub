1ea0e6456bcece5550feec28291335ed19c5b326
Eduardo349 <jonaseduardo879@gmail.com>
2026-02-09 13:31:45 -0300

    fix: provent TMDB config erro during build

--- src/app/movie/[id]/page.tsx
+++ src/app/movie/[id]/page.tsx
@@ -1,34 +1,21 @@
-"use client";
-
-import { useEffect, useState } from "react";
-import { useParams } from "next/navigation";
 import { getMovieById } from "@/services/tmdb";
+import { notFound } from "next/navigation";
 
-export default function MoviePage() {
-  const { id } = useParams();
-  const [movie, setMovie] = useState<any>(null);
+interface Props {
+  params: { id: string };
+}
 
-  useEffect(() => {
-    if (!id) return;
-
-    getMovieById(id as string).then(setMovie);
-  }, [id]);
+export default async function MoviePage({ params }: Props) {
+  const movie = await getMovieById(params.id);
 
   if (!movie) {
-    return <p className="p-6">Carregando...</p>;
+    notFound();
   }
 
   return (
     <main className="p-6">
-      <h1 className="text-3xl font-bold mb-4">{movie.title}</h1>
-
-      <img
-        src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-        alt={movie.title}
-        className="rounded mb-4"
-      />
-
-      <p>{movie.overview}</p>
+      <h1 className="text-3xl font-bold">{movie.title}</h1>
+      <p className="mt-4 text-gray-300">{movie.overview}</p>
     </main>
   );
 }
