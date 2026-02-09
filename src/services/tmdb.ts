85c95b62adde98f0d95946e690b76732a5231579
Eduardo349 <jonaseduardo879@gmail.com>
2026-02-09 13:28:28 -0300

    fix: provent TMDB config erro during build

--- src/services/tmdb.ts
+++ src/services/tmdb.ts
@@ -1,56 +1,41 @@
 const API_KEY = process.env.NEXT_PUBLIC_TMDB_API_KEY;
 const BASE_URL = process.env.NEXT_PUBLIC_TMDB_BASE_URL;
 
-
-
-if (!API_KEY) {
-  console.warn("⚠️ TMDB API KEY não configurada");
-}
-
 export async function getPopularMovies() {
-  const response = await fetch(
-    `${BASE_URL}/movie/popular?language=pt-BR`,
-    {
-      headers: {
-        "Content-Type": "application/json",
-      },
-      cache: "no-store",
-    }
-  );
-
-  if (!response.ok) {
-    throw new Error("Erro ao buscar filmes");
+  if (!API_KEY || !BASE_URL) {
+    return { results: [] };
   }
 
-  return response.json();
+  const res = await fetch(
+    `${BASE_URL}/movie/popular?api_key=${API_KEY}&language=pt-BR`,
+    { cache: "no-store" }
+  );
+
+  return res.json();
 }
 
 export async function getMovieById(id: string) {
-  const response = await fetch(
-    `${BASE_URL}/movie/${id}?api_key=${API_KEY}&language=pt-BR`,
-    {
-      cache: "no-store"
-    }
-  );
-
-  if (!response.ok) {
-    console.error("Erro TMDB:", response.status);
-    return null; // 👈 não quebra a página
+  if (!API_KEY || !BASE_URL) {
+    return null;
   }
 
-  return response.json();
+  const res = await fetch(
+    `${BASE_URL}/movie/${id}?api_key=${API_KEY}&language=pt-BR`,
+    { cache: "no-store" }
+  );
+
+  return res.json();
 }
 
 export async function searchMovies(query: string) {
-  const response = await fetch(
-    `${BASE_URL}/search/movie?api_key=${API_KEY}&language=pt-BR&query=${encodeURIComponent(
-      query
-    )}`
-  );
-
-  if (!response.ok) {
-    throw new Error("Erro ao buscar filmes");
+  if (!API_KEY || !BASE_URL) {
+    return { results: [] };
   }
 
-  return response.json();
+  const res = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${query}&language=pt-BR`,
+    { cache: "no-store" }
+  );
+
+  return res.json();
 }
